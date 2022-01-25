3d4b06e0735c5022cc950f0a08e8f30a0a21b70d
pierwill <pierwill@users.noreply.github.com>
2022-01-25 11:42:33 -0600

    Finish solver for Project Euler #153...
    ... but it's too slow 😔

--- examples/problem-153.rs
+++ examples/problem-153.rs
@@ -3,13 +3,34 @@
 use gaussiant::GaussianInt;
 
 fn main() {
-    let n = 5;
-    let set = gaussiant::get_g_ints(n);
-    for x in set {
-        if x != GaussianInt::zero() && x.0.re != 0 {
-            if x.is_divisor_of(GaussianInt::new(n, 0)) {
-                println!("{x}");
+    let n = 100;
+    let mut big_sum = 0;
+
+    for _n in 1..=n {
+        let set = gaussiant::get_g_ints(_n);
+        let mut divisors = vec![];
+        let mut sum = 0;
+
+        for z in set {
+            if z != GaussianInt::zero() && z.0.re != 0 {
+                if z.is_divisor_of(GaussianInt::new(_n, 0)) {
+                    divisors.push(z);
+                    sum += z.0.re;
+                    sum += z.0.im;
+                    big_sum += z.0.re;
+                    big_sum += z.0.im;
+                }
             }
         }
+
+        // format divisors as string
+        let mut output = String::new();
+        for d in divisors {
+            output.push_str(&(d.to_string() + " "));
+        }
+
+        println!("{_n}: {output} {sum}");
     }
+
+    println!("big sum: {}", big_sum);
 }
