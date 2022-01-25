c00b741d3a1b5071068005e0ec897a81452ed749
pierwill <pierwill@users.noreply.github.com>
2022-01-25 11:13:31 -0600

    Avoid division by zero in `is_divisor_of`

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,11 @@ impl<T: PrimInt + Signed> GaussianInt<T> {
 
     /// Returns `true` if `self` is a divisor of `other`.
     pub fn is_divisor_of(&self, other: Self) -> bool {
-        (other % *self) == Self::zero()
+        if !(other.0.re != T::zero() && other.0.im != T::zero()) {
+            (other % *self) == Self::zero()
+        } else {
+            false
+        }
     }
 
     /// Tests whether a Gaussian integer is a rational integer.
