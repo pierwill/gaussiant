712447d1d6cc6b6431c1054c39c62cdf3abc46b2
pierwill <19642016+pierwill@users.noreply.github.com>
2022-01-30 22:50:56 -0600

    Require num_integer::Integer on integer types

--- src/ops.rs
+++ src/ops.rs
@@ -1,42 +1,43 @@
 use crate::GaussianInt;
+use num_integer::Integer;
 use num_traits::{PrimInt, Signed};
 
-impl<T: PrimInt> std::ops::Add for GaussianInt<T> {
+impl<T: PrimInt + Integer> std::ops::Add for GaussianInt<T> {
     type Output = Self;
     fn add(self, other: Self) -> Self::Output {
         Self::new(self.0.re + other.0.re, self.0.im + other.0.im)
     }
 }
 
-impl<T: PrimInt> std::ops::Sub for GaussianInt<T> {
+impl<T: PrimInt + Integer> std::ops::Sub for GaussianInt<T> {
     type Output = Self;
     fn sub(self, other: Self) -> Self::Output {
         Self::new(self.0.re - other.0.re, self.0.im - other.0.im)
     }
 }
 
-impl<T: PrimInt> std::ops::Mul for GaussianInt<T> {
+impl<T: PrimInt + Integer> std::ops::Mul for GaussianInt<T> {
     type Output = Self;
     fn mul(self, other: Self) -> Self::Output {
         Self::from(self.0 * other.0)
     }
 }
 
-impl<T: PrimInt> std::ops::Div for GaussianInt<T> {
+impl<T: PrimInt + Integer> std::ops::Div for GaussianInt<T> {
     type Output = Self;
     fn div(self, other: Self) -> Self::Output {
         Self::from(self.0 / other.0)
     }
 }
 
-impl<T: PrimInt> std::ops::Rem for GaussianInt<T> {
+impl<T: PrimInt + Integer> std::ops::Rem for GaussianInt<T> {
     type Output = Self;
     fn rem(self, other: Self) -> Self::Output {
         Self::from(self.0 % other.0)
     }
 }
 
-impl<T: PrimInt + Signed> std::ops::Neg for GaussianInt<T> {
+impl<T: PrimInt + Integer + Signed> std::ops::Neg for GaussianInt<T> {
     type Output = Self;
     fn neg(self) -> Self::Output {
         Self::from(-self.0)
