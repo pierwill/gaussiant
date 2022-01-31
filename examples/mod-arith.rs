3e6587ee6385f20564f2a4c7e13bb6bdc18e3fec
pierwill <pierwill@users.noreply.github.com>
2022-01-31 13:54:55 -0600

    Add equivalence relation

--- examples/mod-arith.rs
+++ examples/mod-arith.rs
@@ -12,15 +12,25 @@ fn main() {
     let a2 = gaussint!(4, 1);
     let b2 = gaussint!(4, -3);
 
-    // a1 ≡ b1 (mod n)
+    // a ≡ b (mod n)
     assert!(a.congruent(b, n));
-    // a1 ≡ b1 (mod n)
-    assert!(a1.congruent(b1, n));
     // a2 ≡ b2 (mod n)
     assert!(a2.congruent(b2, n));
 
+    // The congruence relation satisfies all the conditions of an equivalence relation:
+
+    // a ≡ a (mod n) (reflexivity)
+    assert!(a.congruent(a, n));
+
+    // a ≡ b (mod n) if b ≡ a (mod n) for all a, b, and n. (symmetry)
+    assert_eq!(a.congruent(b, n), b.congruent(a, n));
+
+    // a ≡ b (mod n) and b ≡ c (mod n) ⇒ a ≡ c (mod n) (transitivity)
+    let c = gaussint!(5, 4);
+    assert!(b.congruent(c, n));
+    assert!(a.congruent(c, n));
+
     // If a1 ≡ b1 (mod n) and a2 ≡ b2 (mod n), or if a ≡ b (mod n), then:
-    // ==================================================================
 
     // a1 + a2 ≡ b1 + b2 (mod n) (compatibility with addition)
     assert!((a1 + a2).congruent(b1 + b2, n));
@@ -30,8 +40,8 @@ fn main() {
 
     // a + k ≡ b + k (mod n) for any integer k (compatibility with translation)
     let k = gaussint!(10);
-    assert!((a + k).congruent(b + k, n));
 
+    assert!((a + k).congruent(b + k, n));
     // k a ≡ k b (mod n) for any integer k (compatibility with scaling)
     assert!((a * k).congruent(b * k, n));
 
