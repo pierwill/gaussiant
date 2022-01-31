84f9258773d8ed71b92544d2fdfacfb5d63c7158
pierwill <pierwill@users.noreply.github.com>
2022-01-31 13:59:15 -0600

    nice subscripts in comments

--- examples/mod-arith.rs
+++ examples/mod-arith.rs
@@ -14,7 +14,7 @@ fn main() {
 
     // a ≡ b (mod n)
     assert!(a.congruent(b, n));
-    // a2 ≡ b2 (mod n)
+    // a₂ ≡ b₂ (mod n)
     assert!(a2.congruent(b2, n));
 
     // The congruence relation satisfies all the conditions of an equivalence relation:
@@ -30,12 +30,12 @@ fn main() {
     assert!(b.congruent(c, n));
     assert!(a.congruent(c, n));
 
-    // If a1 ≡ b1 (mod n) and a2 ≡ b2 (mod n), or if a ≡ b (mod n), then:
+    // If a₁ ≡ b₁ (mod n) and a₂ ≡ b₂ (mod n), or if a ≡ b (mod n), then:
 
-    // a1 + a2 ≡ b1 + b2 (mod n) (compatibility with addition)
+    // a₁ + a₂ ≡ b₁ + b₂ (mod n) (compatibility with addition)
     assert!((a1 + a2).congruent(b1 + b2, n));
 
-    // a1 – a2 ≡ b1 – b2 (mod n) (compatibility with subtraction)
+    // a₁ – a₂ ≡ b₁ – b₂ (mod n) (compatibility with subtraction)
     assert!((a1 - a2).congruent(b1 - b2, n));
 
     // a + k ≡ b + k (mod n) for any integer k (compatibility with translation)
@@ -45,13 +45,13 @@ fn main() {
     // k a ≡ k b (mod n) for any integer k (compatibility with scaling)
     assert!((a * k).congruent(b * k, n));
 
-    // a1 + a2 ≡ b1 + b2 (mod n) (compatibility with addition)
+    // a₁ + a₂ ≡ b₁ + b₂ (mod n) (compatibility with addition)
     assert!((a1 + a2).congruent(b1 + b2, n));
 
-    // a1 – a2 ≡ b1 – b2 (mod n) (compatibility with subtraction)
+    // a₁ – a₂ ≡ b₁ – b₂ (mod n) (compatibility with subtraction)
     assert!((a1 + a2).congruent(b1 + b2, n));
 
-    // a1 a2 ≡ b1 b2 (mod n) (compatibility with multiplication)
+    // a₁ a₂ ≡ b₁ b₂ (mod n) (compatibility with multiplication)
     assert!((a1 * a2).congruent(b1 * b2, n));
 
     // a^k ≡ b^k (mod n) for any non-negative integer k (compatibility with exponentiation)
