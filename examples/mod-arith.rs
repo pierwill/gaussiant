12e0eca50e8e16476f2df755a2bc7569cf19c27d
pierwill <pierwill@users.noreply.github.com>
2022-01-31 12:50:31 -0600

    Add example demonstrating properties of modular arithmetic

---
+++ examples/mod-arith.rs
@@ -0,0 +1,53 @@
+//! See <https://en.wikipedia.org/wiki/Modular_arithmetic#Properties>.
+
+use gaussiant::{gaussint, GaussianInt};
+
+fn main() {
+    let a = gaussint!(10);
+    let b = gaussint!(7);
+    let n = gaussint!(3);
+
+    let a1 = a;
+    let b1 = b;
+    let a2 = gaussint!(1);
+    let b2 = gaussint!(4);
+
+    // a1 ≡ b1 (mod n)
+    assert!(a.congruent(b, n));
+    // a1 ≡ b1 (mod n)
+    assert!(a1.congruent(b1, n));
+    // a2 ≡ b2 (mod n)
+    assert!(a2.congruent(b2, n));
+
+    // If a1 ≡ b1 (mod n) and a2 ≡ b2 (mod n), or if a ≡ b (mod n), then:
+    // ==================================================================
+
+    // a1 + a2 ≡ b1 + b2 (mod n) (compatibility with addition)
+    assert!((a1 + a2).congruent(b1 + b2, n));
+
+    // a1 – a2 ≡ b1 – b2 (mod n) (compatibility with subtraction)
+    assert!((a1 - a2).congruent(b1 - b2, n));
+
+    // a + k ≡ b + k (mod n) for any integer k (compatibility with translation)
+    let k = gaussint!(10);
+    assert!((a + k).congruent(b + k, n));
+
+    // k a ≡ k b (mod n) for any integer k (compatibility with scaling)
+    assert!((a * k).congruent(b * k, n));
+
+    // a1 + a2 ≡ b1 + b2 (mod n) (compatibility with addition)
+    assert!((a1 + a2).congruent(b1 + b2, n));
+
+    // a1 – a2 ≡ b1 – b2 (mod n) (compatibility with subtraction)
+    assert!((a1 + a2).congruent(b1 + b2, n));
+
+    // a1 a2 ≡ b1 b2 (mod n) (compatibility with multiplication)
+    assert!((a1 * a2).congruent(b1 * b2, n));
+
+    // a^k ≡ b^k (mod n) for any non-negative integer k (compatibility with exponentiation)
+    // TODO
+
+    // p(a) ≡ p(b) (mod n), for any polynomial p(x) with integer coefficients
+    // (compatibility with polynomial evaluation)
+    // TODO?
+}
